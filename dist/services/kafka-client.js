8c843c8386b0d0cc15153bc70d92d15db5c49047
suraj <suraj222615@gmail.com>
2024-09-03 13:26:07 +0530

    Revert "final"
    This reverts commit fb31665c67c622b4af113ddfa3015446a22daeb2.

--- dist/services/kafka-client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kafka = void 0;
-const kafkajs_1 = require("kafkajs");
-exports.kafka = new kafkajs_1.Kafka({
-    brokers: [`${process.env.KAFKA_USER_IP}:${process.env.KAFKA_PORT}`],
-    clientId: "ting-tong"
-});
--- src/utils/connectDb.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-
-export function connectDb(dbUrl:string){
-    console.log(dbUrl)
-    mongoose.connect(dbUrl,{
-        dbName:"chatapp"
-    }).then((db)=>console.log(db.connection.host)).catch((err)=>console.log(err))  
-}
